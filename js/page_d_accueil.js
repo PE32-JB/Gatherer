af5cb194c6160ff20d90b2206e7929e9f0456986
PE32 <jean-baptiste.pottier@ecl13.ec-lyon.fr>
2013-12-18 15:09:34 +0100

    Create page_d_accueil.js

---
+++ js/page_d_accueil.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".widget").mouseenter(function(){
+		$(this).find(".text_case").slideToggle(400);
+	})
+	$(".widget").mouseleave(function(){
+		$(this).find(".text_case").slideToggle(400);
+	})
+})
