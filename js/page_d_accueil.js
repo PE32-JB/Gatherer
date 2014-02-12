79b2260ced773009a54185f18438c90b1ac8602d
PE32 <jean-baptiste.pottier@ecl13.ec-lyon.fr>
2014-02-12 14:42:35 +0100

    Suppression des fichiers obsolètes

--- js/page_d_accueil.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".widget").mouseenter(function(){
-		$(this).find(".text_case").slideToggle(400);
-	})
-	$(".widget").mouseleave(function(){
-		$(this).find(".text_case").slideToggle(400);
-	})
-})
