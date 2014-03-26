0d37fdfb1f1acaedea7a9f11120f40561d0b9fe9
PE32 <jean-baptiste.pottier@ecl13.ec-lyon.fr>
2014-03-26 16:48:22 +0100

    Revert "Changement du style"
    This reverts commit e9d6933c8ccc47b2888ec9f42d16e5e2868e59eb.

--- version-docpad/src/documents/scripts/canvas.js
+++ version-docpad/src/documents/scripts/canvas.js
@@ -1,43 +1,20 @@
-	var x=190;
+	var x=100;
 	var rect = document.getElementById("mon_canvas");
 	var ctx = rect.getContext("2d");
-	//-----------------------------------------
-	//gestion du dégradé
-	var lineGrad=ctx.createLinearGradient(10,25,200,25);
-	lineGrad.addColorStop(0,'rgba(0,128,0,0.2)');
-	lineGrad.addColorStop(1,'green');
-	//------------------------------------------
-	//remplissage
-	ctx.beginPath();
-	ctx.fillStyle=lineGrad;
-	ctx.moveTo(10,10);    // Le tracé part du point 50,50
-	ctx.lineTo(x,10);   
-	ctx.lineTo(x,30);   
-	ctx.lineTo(10,30); 	
-	ctx.lineTo(10,10);
-	ctx.closePath();      // Fermeture du chemin (facultative)
-	ctx.strokeStyle='black';
-	ctx.fill();
-	//------------------------------------------
-	//flèche extérieure
-	ctx.beginPath();
-	ctx.moveTo(10,10);   
+					ctx.beginPath();
+					ctx.moveTo(10,10);    // Le tracÃ© part du point 50,50
+					ctx.lineTo(x,10);   
+					ctx.lineTo(x,40);   
+					ctx.lineTo(10,40); 	
+					ctx.lineTo(10,10);
+					ctx.closePath();      // Fermeture du chemin (facultative)
+					ctx.stroke();
+					ctx.fill();
+					ctx.beginPath();      // DÃ©but du chemin
+					ctx.moveTo(10,10);    // Le tracÃ© part du point 50,50
 	ctx.lineTo(200,10);  
-	ctx.lineTo(200,0);
-	ctx.lineTo(230,20);
-	ctx.lineTo(200,40);
-	ctx.lineTo(200,30);   
-	ctx.lineTo(10,30); 	
+	ctx.lineTo(200,40);   
+	ctx.lineTo(10,40); 	
 	ctx.lineTo(10,10);
 	ctx.closePath();      // Fermeture du chemin (facultative)
 	ctx.stroke();
-	//--------------------------------------
-	//curseur
-	ctx.beginPath();
-	ctx.moveTo(x,42);
-	ctx.lineTo(x+8, 50);
-	ctx.lineTo(x-8, 50);
-	ctx.closePath();
-	ctx.fill();
-	ctx.fillStyle='black';
-	ctx.fillText("Avancement", x-28, 60);
